204f1e5926d58ce2a6f9b7e5afc20907bb4ab4f3
Dima <d.d.demin@urfu.me>
2020-08-31 12:48:37 +0500

    Question is registered

--- djangoProjectMysite/polls/models.py
+++ djangoProjectMysite/polls/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.question_text
 
 
 
@@ -10,4 +16,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 # Create your models here.
